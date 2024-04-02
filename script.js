006a3c28829b6d754307794adae8d4b99ede421a
poornima reddy <54319456+Poornimait@users.noreply.github.com>
2024-04-02 11:19:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+	const firstDate = new Date(date1);
+	const secondDate = new Date(date2);
+	const diffMilliseconds = secondDate - firstDate;
+	const diffDays = diffMilliseconds / (1000 * 60 * 60 * 24);
+    return Math.round(diffDays);
 };
 
 // Do not change the code below.
